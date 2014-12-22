2dedd51a324b1aa27334507fc82d2d8f8c45cdba
Evgeny <e.nazarov@yahoo.com>
2014-12-22 21:48:40 +0500

    Initial Commit

---
+++ ENSpinnerView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ENSpinnerView
+//
+//  Created by Evgeny on 22.12.14.
+//  Copyright (c) 2014 Evgeny Nazarov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ENSpinnerView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ENSpinnerView
+//
+//  Created by Evgeny on 22.12.14.
+//  Copyright (c) 2014 Evgeny Nazarov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
